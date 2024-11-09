7ef0db4e9c295dc39bae4066c5fd13e9194a8f80
VanwTus09 <tunguyen/05112004@gmail.com>
2024-11-09 23:46:29 +0700

    Remove .git from public folder

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: false, // Bỏ qua thư mục public .git
+  // publicDir: false, // Bỏ qua thư mục public .git
   base: '/',
   build: {
     outDir: 'dist', // Thư mục xuất ra
-    rollupOptions: {
-      external: ['**/.git/**'], // Bỏ qua tất cả các file trong .git
-    },
+    // rollupOptions: {
+    //   external: ['**/.git/**'], // Bỏ qua tất cả các file trong .git
+    // },
   },
 });

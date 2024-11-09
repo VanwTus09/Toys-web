9f43dc2dd3878fae7d5ba3d06a6b34f291c3153a
VanwTus09 <tunguyen/05112004@gmail.com>
2024-11-09 23:05:57 +0700

    Update build

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   publicDir: false, // Bỏ qua thư mục public .git
+  base: '/',
   build: {
     outDir: 'dist', // Thư mục xuất ra
-
     rollupOptions: {
       external: ['**/.git/**'], // Bỏ qua tất cả các file trong .git
     },

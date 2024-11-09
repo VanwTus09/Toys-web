c8e66624f734bdb47cdb217bda0cb59d3955442f
VanwTus09 <tunguyen/05112004@gmail.com>
2024-11-09 21:55:35 +0700

    deploy1

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/toys-web/', // Tên repo của bạn trên GitHub
+  base: '/toys-web/',  // Đảm bảo base path đúng với cấu trúc URL trên Vercel
   publicDir: false, // Bỏ qua thư mục public .git
   build: {
+    outDir: 'dist', // Thư mục xuất ra
+
     rollupOptions: {
       external: ['**/.git/**'], // Bỏ qua tất cả các file trong .git
     },
